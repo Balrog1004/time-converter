25ac4093a3d0db817e43e467740003181b78e041
balrog1004 <lmkj0051@gmail.com>
2022-07-29 17:13:43 +0900

    time_converter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+function App () {
+    const [amount,setAmount] = useState(0);
+    const [active,setActive] = useState(true);
+
+    const onChange = (event) =>{
+        console.log(event);
+
+        setAmount(event.target.value);    
+      };
+    const reset = () => {
+        setAmount(0);
+    };
+    const changeActive = () => {
+      reset();
+      setActive((prevState) => {
+        return !prevState;
+      });
+    };
+
+    return (
+        <div>
+            <div>time Converter</div>
+            <div>
+                minutes{" "}
+                <input
+                    placeholder="Minutes"
+                    Value={active ? amount : amount * 60}
+                    type="number"
+                    onChange={onChange}
+                    disabled={!active}
+                />
+                <div>
+                    Hours{" "}
+                    <input
+                    type="number"
+                    placeholder="Hours"
+                    Value={active ? Math.floor(amount/60) : amount}
+                    disabled={active}
+                    onChange={onChange}
+                />
+                </div>
+                <button onClick={reset}>reset</button>
+                <button onClick={changeActive}>active toggle</button>
+            </div>
+            <hr />
+        </div>
+    );
+};
 
 export default App;
